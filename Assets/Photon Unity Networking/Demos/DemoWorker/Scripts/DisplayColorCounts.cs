4228c6cea7bed060ce81a53dbaafcdc56932d217
Harry Xian <soraxian@gmail.com>
2015-04-06 18:14:23 -0700

    Add display name for player. and fix the yellow score.

--- Assets/Photon Unity Networking/Demos/DemoWorker/Scripts/DisplayColorCounts.cs
+++ Assets/Photon Unity Networking/Demos/DemoWorker/Scripts/DisplayColorCounts.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class DisplayColorCounts : MonoBehaviour {
 	
 	public Text countText;
+	public int playerNum;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,14 +13,48 @@ public class DisplayColorCounts : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		countText.text = "" + Grid.colors[Color.blue] + " ";
-		
-		countText.text += "" + Grid.colors[Color.red] + " ";
-		
-		countText.text += "" + Grid.colors[Color.green]+ " ";
-		
-		countText.text += "" + Grid.colors[Color.black]+ " ";
-		
+		string[] playerNames = new string[4];
+		foreach(PhotonPlayer a in PhotonNetwork.playerList){
+			if(a.ID == 1){
+				playerNames[0] = a.name;
+			}
+			if(a.ID == 2){
+				playerNames[1] = a.name;
+			}
+			if(a.ID == 3){
+				playerNames[2] = a.name;
+			}
+			if(a.ID == 4){
+				playerNames[3] = a.name;
+			}
+		}
+
+		switch (playerNum) {
+		case 1:
+			countText.text = playerNames[0] + " : " + Grid.colors[Color.blue] + " ";
+			countText.color = Color.blue;
+			break;
+		case 2:
+			countText.text = playerNames[1] + " : " + Grid.colors[Color.red] + " ";
+			countText.color = Color.red;
+			break;
+		case 3:
+			countText.text = playerNames[2] + " : " + Grid.colors[Color.green] + " ";
+			countText.color = Color.green;
+			break;
+		case 4:
+			countText.text = playerNames[3] + " : " + Grid.colors[Color.yellow] + " ";
+			countText.color = Color.yellow;
+			break;
+		}
+
+
+//		countText.text = "" + Grid.colors[Color.blue] + " ";
+//		
+//		countText.text += "" + Grid.colors[Color.red] + " ";
+//		
+//		countText.text += "" + Grid.colors[Color.green]+ " ";
+//		
+//		countText.text += "" + Grid.colors[Color.yellow]+ " ";
 	}
 }
